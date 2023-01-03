69abab63793385c88e17f3501656a36c8b21595d
delasimensah <mensadelasi@gmail.com>
2023-01-03 17:05:09 +0000

    added all pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
-const Home = () => {
-  return <main>Homepage</main>;
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <main className="">
+      Homepage
+      <Link
+        href={{
+          pathname: "/[username]",
+          query: { username: "delasim" },
+        }}
+      >
+        Delasi Profile
+      </Link>
+    </main>
+  );
 };
 
-export default Home;
+export default HomePage;
